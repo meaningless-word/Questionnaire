ce03c933c44247bbb58f63bbb02d1911fc3bd3e6
Роман Толмачёв <unifiq@gmail.com>
2020-12-17 22:41:51 +0700

    Добавьте файлы проекта.

---
+++ Questionnaire/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Questionnaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Представьтесь, пожалуйста: ");
+            string name = Console.ReadLine();
+            Console.Write("Укажите свой возраст: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Укажите дату рождения: ");
+            var birthday = Console.ReadLine();
+
+            Console.WriteLine("Подтвердите правильность информации:");
+            Console.WriteLine($"Ваше имя {name}");
+            Console.WriteLine("Дата рождения {0}", birthday);
+            Console.WriteLine("Полных лет: " + age.ToString());
+
+            Console.ReadKey();
+
+        }
+    }
+}
