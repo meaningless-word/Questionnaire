c4e2164631a5111a3244b425c41afb15c2e724cb
Роман Толмачёв <unifiq@gmail.com>
2020-12-24 23:16:18 +0700

    добваление инициализации кортежа

--- Questionnaire/Program.cs
+++ Questionnaire/Program.cs
@@ -20,6 +20,26 @@ namespace Questionnaire
 
             Console.ReadKey();
 
+            //кортеж
+            (string name, int age) anketa;
+            Console.Write("Представьтесь, пожалуйста: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Укажите свой возраст: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Подтвердите правильность информации:");
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+
+            (string Name, string Type, double Age, int NameCount) Pet;
+            Console.WriteLine("Введите имя питомца");
+            Pet.Name = Console.ReadLine();
+            Pet.NameCount = Pet.Name.Length;
+
+            Console.WriteLine("Введите возраст питомца");
+            Pet.Age = double.Parse(Console.ReadLine());
+
+
         }
     }
 }
